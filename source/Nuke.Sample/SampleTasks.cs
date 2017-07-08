76e8c75f87f46a8eac840955665da243cb8e9c4f
Matthias Koch <ithrowexceptions@gmail.com>
2017-07-08 10:23:15 +0200

    Add SampleTasks.

---
+++ source/Nuke.Sample/SampleTasks.cs
@@ -0,0 +1,25 @@
+﻿// Copyright Matthias Koch 2017.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/sample/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using JetBrains.Annotations;
+using Nuke.Core;
+using Nuke.Core.Execution;
+using Nuke.Sample;
+
+[assembly: IconClass(typeof(SampleTasks), "power-cord2")]
+
+namespace Nuke.Sample
+{
+    [PublicAPI]
+    public class SampleTasks
+    {
+        public static void LogConfiguration()
+        {
+            var configuration = Build.Instance?.Configuration;
+            Logger.Info($"Build is running in {configuration}.");
+        }
+    }
+}
