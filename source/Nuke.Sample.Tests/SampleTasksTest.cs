cc91f68afde7a7e253a459152133c6aa69339470
Matthias Koch <ithrowexceptions@gmail.com>
2017-07-23 04:01:03 +0200

    Add test project.

---
+++ source/Nuke.Sample.Tests/SampleTasksTest.cs
@@ -0,0 +1,18 @@
+﻿// Copyright Matthias Koch 2017.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/sample/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Nuke.Sample.Tests
+{
+    public class SampleTasksTest
+    {
+        [Fact]
+        public void Test()
+        {
+        }
+    }
+}
